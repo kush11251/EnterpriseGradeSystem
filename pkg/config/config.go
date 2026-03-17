ffb2a99f2ca4ba194ae8d4beac5fbb39946f4853
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-18 01:56:37 +0530

    feat: integrate pkg/config/config.go

---
+++ pkg/config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Config struct {
+	Database struct {
+		Host     string `json:"host"`
+		Port     int    `json:"port"`
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+}
+
+func LoadConfig(filename string) (*Config, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	var config Config
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
